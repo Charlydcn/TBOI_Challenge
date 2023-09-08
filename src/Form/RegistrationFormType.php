2ef17fc64eab016ded32f7c58d3027f86947e88a
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-08 20:47:22 +0200

    RegistrationForm: added all inputs+labels

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,46 +4,68 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add('email', EmailType::class, [
+                "label" => "E-Mail : ",
+                "attr" => [
+                    "placeholder" => "john.doe@example.fr",
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+            ->add('username', TextType::class, [
+                "label" => "Username : ",
+                "attr" => [
+                    "placeholder" => "McMillen1337",
+                ],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+                        'min' => 3,
+                        'minMessage' => 'Usernames have to be atleast 3 characters',
+                        'max' => 20,
+                        'maxMessage' => 'Usernames can\'t exceed 20 characters',
+                    ])
+                ]
             ])
-        ;
+            ->add('password', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Passwords don\'t match',
+                'required' => true,
+                'first_options'  => [
+                    'label' => 'Password : ',
+                    "attr" => [
+                        "placeholder" => "••••••••",
+                        ]
+                    ],
+                'second_options' => [
+                    'label' => 'Password confirmation : ',
+                    "attr" => [
+                        "placeholder" => "••••••••",
+                        ]
+                    ],
+                'constraints' => [
+                    new Length([
+                            'min' => 8,
+                            'minMessage' => 'Passwords have to be atleast 8 characters',
+                            'max' => 30,
+                            'maxMessage' => 'Passwords can\'t exceed 30 characters',
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
