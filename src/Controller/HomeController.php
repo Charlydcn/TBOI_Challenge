21b506efe6a82dc91c099c2897f3478e40617c22
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-10 19:38:27 +0200

    HomeCtrl: added route '/'

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,10 +13,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    #[Route('/', name: 'app_home')]
+    #[Route('/')]
     public function index(CharacterRepository $characterRepository, BossRepository $bossRepository, RestrictionRepository $restrictionRepository): Response
     {
-
         $characters = $characterRepository->findAll();
         $bosses = $bossRepository->findAll();
         $restrictions = $restrictionRepository->findAll();
