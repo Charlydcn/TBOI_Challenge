dcb23ed9915ae3be72ff47c6794e37b760cb1508
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-12 08:31:37 +0200

    script.js:options menu script file will be renamed

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,58 @@
-// open step 1 on 'options' btn, close it
 const optionsBtn = document.querySelector('#options');
 const closeBtn = document.querySelector('#close-btn');
 const form = document.querySelector('#form');
 const overlay = document.querySelector('#overlay');
+const nextBtn = document.querySelector('#next-btn');
+const step1 = document.querySelector('#step1');
+const step2 = document.querySelector('#step2');
+const previousBtn = document.querySelector('#previous-btn')
+const submit = form.lastElementChild
 
+// previousBtn on click : show step1 and hide step2
+previousBtn.addEventListener('click', () => {
+    step1.classList.remove('hidden')
+    step2.classList.add('hidden')
+})
+
+// options on click : show form, step 1 and overlay
 optionsBtn.addEventListener('click', () => {
     form.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+    step1.classList.remove('hidden');
+    overlay.classList.remove('hidden')
 });
 
+// closeBtn on click : hide everything
 closeBtn.addEventListener('click', () => {
-    form.classList.add('hidden');
-    overlay.classList.add('hidden');
+    hideFormAndSteps();
 });
 
+// overlay on click : hide form and reset step
 overlay.addEventListener('click', (event) => {
-    // Vérifie si la cible du clic est l'overlay lui-même et non le formulaire
     if (event.target === overlay) {
-        form.classList.add('hidden');
-        overlay.classList.add('hidden');
+        hideFormAndSteps();
     }
 });
+
+// nextBtn on click : hide step1 and show step2 + show submit btn
+nextBtn.addEventListener('click', () => {
+    step1.classList.add('hidden');
+    step2.classList.remove('hidden');
+});
+
+// close btn
+closeBtn.addEventListener('click', resetAndHideForm);
+
+// hidden on everything + step 1 not hidden
+function resetAndHideForm() {
+    hideFormAndSteps();
+    step1.classList.remove('hidden');
+}
+
+// put hidden on everything
+function hideFormAndSteps() {
+    form.classList.add('hidden');
+    step1.classList.add('hidden');
+    step2.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
+
