1e5111226916751aea2ead5700d1626833d9f9b5
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-20 10:24:58 +0200

    Ent/Friendship: new entity for friendships

---
+++ src/Entity/Friendship.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FriendshipRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FriendshipRepository::class)]
+class Friendship
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'friendships')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user1 = null;
+
+    #[ORM\ManyToOne(inversedBy: 'friendships')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user2 = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser1(): ?User
+    {
+        return $this->user1;
+    }
+
+    public function setUser1(?User $user1): static
+    {
+        $this->user1 = $user1;
+
+        return $this;
+    }
+
+    public function getUser2(): ?User
+    {
+        return $this->user2;
+    }
+
+    public function setUser2(?User $user2): static
+    {
+        $this->user2 = $user2;
+
+        return $this;
+    }
+}
