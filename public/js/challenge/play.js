f515edb80d791227935ecf8140374fcf9e30977c
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-22 17:16:08 +0200

    challenge/play.js: play script for win streaks

---
+++ public/js/challenge/play.js
@@ -0,0 +1,33 @@
+// win counter script --------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------
+
+// set count to 0
+let count = 0
+const challengeStreak = document.getElementById('challenge-streak')
+const winCounter = document.querySelector("#win-counter p")
+
+
+function increment() {
+    // +1 to count
+    count++
+
+    // update counter text
+    winCounter.textContent = count
+
+    // check if current win-streak is enough to win the challenge
+    checkCount()
+
+    // remove class hidden of win-streak div when user won atleast 1 time
+    if (winCounter.parentElement.classList.contains('hidden')) {
+        winCounter.parentElement.classList.remove('hidden')
+    }
+}
+
+function checkCount() {
+    // if nb of user completion of the challenge match the required challenge win-streak
+    if (count == challengeStreak.textContent) {
+        // if so, redirect by getting the data-href in winCounter parent div
+        var url = winCounter.parentElement.getAttribute('data-href')
+        window.location.href = url
+    }
+}
