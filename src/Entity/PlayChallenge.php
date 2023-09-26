66fe755e9e992109d77c1e71a34ef867b7d1a4be
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-26 15:06:37 +0200

    Ent/PlayChall: new completionTime (time_mutable)

--- src/Entity/PlayChallenge.php
+++ src/Entity/PlayChallenge.php
@@ -17,19 +17,19 @@ class PlayChallenge
     #[ORM\Column]
     private ?bool $completed = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $completionTime = null;
-
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $playDate = null;
+
+    #[ORM\ManyToOne(inversedBy: 'challengesPlayed')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
 
     #[ORM\ManyToOne(inversedBy: 'players')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Challenge $challenge = null;
 
-    #[ORM\ManyToOne(inversedBy: 'challengesPlayed')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $user = null;
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $completionTime = null;
 
     public function getId(): ?int
     {
@@ -48,18 +48,6 @@ class PlayChallenge
         return $this;
     }
 
-    public function getCompletionTime(): ?\DateTimeInterface
-    {
-        return $this->completionTime;
-    }
-
-    public function setCompletionTime(?\DateTimeInterface $completionTime): static
-    {
-        $this->completionTime = $completionTime;
-
-        return $this;
-    }
-
     public function getPlayDate(): ?\DateTimeInterface
     {
         return $this->playDate;
@@ -68,18 +56,6 @@ class PlayChallenge
     public function setPlayDate(\DateTimeInterface $playDate): static
     {
         $this->playDate = $playDate;
-
-        return $this;
-    }
-
-    public function getChallenge(): ?Challenge
-    {
-        return $this->challenge;
-    }
-
-    public function setChallenge(?Challenge $challenge): static
-    {
-        $this->challenge = $challenge;
 
         return $this;
     }
@@ -99,12 +75,36 @@ class PlayChallenge
     public function __toString()
     {
         $r = "";
-        $r .= "User : " . $this->user . "<br>";
-        $r .= "Last challenge run : " . $this->playDate->format('d-m-Y h:i:s') . "<br>";
-        $r .= "Completed : " . ($this->completed === true ? "Yes" : "No") . "<br>";
-        $r .= $this->completed === true ? "Completion Time : " . $this->completionTime->format('h:i:s') : "";
+        $r .= "User : " . $this->user . " ";
+        $r .= "Last challenge run : " . $this->playDate->format('d-m-Y h:i:s') . " ";
+        $r .= "Completed : " . ($this->completed === true ? "Yes" : "No") . " ";
+        $r .= $this->completed === true ? "Completion Time : " . $this->completionTime->format('h:i:s') : "" . " ";
 
         return $r;
 
+    }
+
+    public function getChallenge(): ?Challenge
+    {
+        return $this->challenge;
+    }
+
+    public function setChallenge(?Challenge $challenge): static
+    {
+        $this->challenge = $challenge;
+
+        return $this;
+    }
+
+    public function getCompletionTime(): ?\DateTimeInterface
+    {
+        return $this->completionTime;
+    }
+
+    public function setCompletionTime(?\DateTimeInterface $completionTime): static
+    {
+        $this->completionTime = $completionTime;
+
+        return $this;
     }
 }
