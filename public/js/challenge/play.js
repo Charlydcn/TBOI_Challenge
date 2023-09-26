9e88afe45a807a85182fc198b701751c361681d6
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-26 15:04:13 +0200

    play.js: comment + hoverInfos show on click script

--- public/js/challenge/play.js
+++ public/js/challenge/play.js
@@ -1,33 +1,98 @@
-// win counter script --------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------------
+// WIN/LOOSE SCRIPT ----------------------------------------------------------------------------------
 
-// set count to 0
-let count = 0
+// = challenge win-streak : <p>
 const challengeStreak = document.getElementById('challenge-streak')
+ // user win-streak : <div>..<p>
 const winCounter = document.querySelector("#win-counter p")
+// <div id="btns"> containing WIN & LOOSE btns
+const btnsDiv = document.getElementById("btns")
+// <div id="completion-time"> containing playChallenge creation form (for completion time)
+const completionTimeForm = document.getElementById("completion-time")
 
+
+let count = 0
 
 function increment() {
-    // +1 to count
-    count++
+    // if challenge has a win-streak condition
+    if (challengeStreak.textContent > 0) {
+        
+        // show win-streak div (flame.webp + counter)
+        if (winCounter.parentElement.classList.contains('hidden')) {
+            winCounter.parentElement.classList.remove('hidden')
+        }
 
-    // update counter text
-    winCounter.textContent = count
+        // if user win-streak isn't enough to win the challenge
+        if (winCounter.textContent !== challengeStreak.textContent) {
+            // increment user win-streak and refresh #win-counter p textContent
+            count++
+            winCounter.textContent = count
+        }
 
-    // check if current win-streak is enough to win the challenge
-    checkCount()
+        // if user win-streak match the challenge win-streak condition
+        if (count >= challengeStreak.textContent) {
+            // call CompletionTime() to show completion time input
+            showCompletionTime()
+        }
 
-    // remove class hidden of win-streak div when user won atleast 1 time
-    if (winCounter.parentElement.classList.contains('hidden')) {
-        winCounter.parentElement.classList.remove('hidden')
+    // else, call CompletionTime() to show completion time input (user won, no win-streak so no multiple runs needed)
+    } else {
+        showCompletionTime()
     }
 }
 
-function checkCount() {
-    // if nb of user completion of the challenge match the required challenge win-streak
-    if (count >= challengeStreak.textContent) {
-        // if so, redirect by getting the data-href in winCounter parent div
-        var url = winCounter.parentElement.getAttribute('data-href')
-        window.location.href = url
-    }
+function showCompletionTime() {
+    // hide btns (WIN & LOOSE)
+    btnsDiv.classList.add('hidden')
+
+    // show completion time input
+    completionTimeForm.classList.remove('hidden')
+
 }
+
+function showBtns() {
+    // show btns (WIN & LOOSE)
+    btnsDiv.classList.remove('hidden')
+
+    // hide completion time input
+    completionTimeForm.classList.add('hidden')
+}
+
+
+// ---------------------------------------------------------------------------------------------------
+// INFOS BOX ON HOVER (RESTRICTIONS) -----------------------------------------------------------------
+
+// restrictions icon : <div id="restrictions">..<div>..<img>
+const restrictionsImgs = document.querySelectorAll('#restrictions div img')
+// infos box : <div class="hover-infos">
+const hoverInfos = document.querySelectorAll(".hover-infos")
+
+// for restriction img
+restrictionsImgs.forEach(restrictionImg => {
+    // when clicked,
+    restrictionImg.addEventListener('click', () => {
+
+        // hide every infos box (hoverInfos) 
+        hoverInfos.forEach(infos => {
+            // except for the closest hoverInfos from the restriction img that user clicked on, structure:
+            // <div>..<img> ~ <div class="hover-infos">..</div> 
+            if(infos !== restrictionImg.nextElementSibling) {
+                infos.classList.add('hidden')
+            }
+        })
+
+        // show/hide closest infos box (hoverInfos) from the restriction img that user clicked on
+        restrictionImg.nextElementSibling.classList.toggle('hidden')
+    })
+})
+
+// if user click anywhere on the document
+document.addEventListener('click', (event) => {
+    // as long as it's not '.hover-infos' and not #restrictions
+    if (!event.target.classList.contains('hover-infos') && !event.target.closest('#restrictions')) {
+        // hide all hoverInfos
+        hoverInfos.forEach(info => {
+            info.classList.add('hidden')
+        })
+    }
+})
