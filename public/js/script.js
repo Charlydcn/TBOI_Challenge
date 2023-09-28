6c64b03b8ff160f6ac195061688cb73e4af2cd06
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-28 15:37:03 +0200

    script.js: + infos box on click (old hover-infos)

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
-// Flash messages disappearance
+// ---------------------------------------------------------------------------------------------------
+// FLASH MSG DISAPPEARANCE ---------------------------------------------------------------------------
 $(document).ready(function() {
     var i = 2000
     $(this).delay(1000)
@@ -12,16 +13,38 @@ $(document).ready(function() {
     })
 })
 
-// href redirection on elements
-var hrefElements = document.querySelectorAll('.href-element');
+// ---------------------------------------------------------------------------------------------------
+// HREF REDIRECTION ON ELEMENTS ----------------------------------------------------------------------
+var hrefElements = document.querySelectorAll('.href-element')
 
 hrefElements.forEach(function (hrefElement) {
     hrefElement.addEventListener('click', function(event) {
         // get data-href attribute which is : <div class="href-element" data-href="example/route...">
-        var url = this.getAttribute('data-href');
-        window.location.href = url;
+        var url = this.getAttribute('data-href')
+        window.location.href = url
 
 
-        event.stopPropagation();
-    });
+        event.stopPropagation()
+    })
+})
+
+// ---------------------------------------------------------------------------------------------------
+// INFOS BOX ON CLICK --------------------------------------------------------------------------------
+var onclickInfosElements = $('.onclick-infos-element')
+var onclickInfos = $('.onclick-infos')
+
+onclickInfosElements.on('click', function(event) {
+    $.each(onclickInfos, function() {
+        if ($(this) !== $(event.currentTarget)) {
+            $(this).addClass('hidden')
+        }
+    })
+
+    $(this).next().toggleClass('hidden')
+})
+
+$(document).on('click', function(event) {
+    if (!$(event.target).is(onclickInfos) && !$(event.target).is(onclickInfosElements)) {
+        $('.onclick-infos').addClass('hidden');
+    }
 });
