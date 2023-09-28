3ee7ba955007a291e1ea2754ab5b79e605323552
Charlydcn <Charly.ducournau@outlook.fr>
2023-09-28 09:01:47 +0200

    script.js: event.stopPropagation for nav sub-menu

--- public/js/script.js
+++ public/js/script.js
@@ -16,8 +16,12 @@ $(document).ready(function() {
 var hrefElements = document.querySelectorAll('.href-element');
 
 hrefElements.forEach(function (hrefElement) {
-    hrefElement.addEventListener('click', function () {
+    hrefElement.addEventListener('click', function(event) {
+        // get data-href attribute which is : <div class="href-element" data-href="example/route...">
         var url = this.getAttribute('data-href');
         window.location.href = url;
+
+
+        event.stopPropagation();
     });
 });
