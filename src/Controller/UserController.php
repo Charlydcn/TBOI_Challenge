e8731c7d70dd4d669a3e3fc6214c50e2d007a56f
Charlydcn <Charly.ducournau@outlook.fr>
2023-11-18 17:36:28 +0100

    userCtrl: show user profile + win streaks

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\VersusRepository;
+use App\Repository\PlayChallengeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/profile/{id}', name: 'show_user')]
+    public function show(
+        User $user,
+        PlayChallengeRepository $playChallengeRepository,
+        VersusRepository $versusRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+
+        $challengesPlayed = count($user->getChallengesPlayed());
+        $challengesWon = count($playChallengeRepository->findBy(['user' => $user, 'completed' => true]));
+        $x = ($challengesWon / $challengesPlayed) * 100;
+        $challengeWinRate = number_format($x, 2);
+
+        $versusPlayed = count($user->getVersusPlayed());
+        $versusWon = count($versusRepository->findBy(['winner' => $user]));
+        $y = ($versusWon / $versusPlayed) * 100;
+        $versusWinRate = number_format($y, 2);
+
+
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+            'challengeWinRate' => $challengeWinRate,
+            'versusWinRate' => $versusWinRate,
         ]);
     }
 }
